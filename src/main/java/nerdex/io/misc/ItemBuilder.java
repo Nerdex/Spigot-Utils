8d38d2b0d44aa81c4ed895a88f987ef6f987dd9f
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-18 00:33:23 +0100

    itembuilder forgot i had that in a previous project

---
+++ src/main/java/nerdex/io/misc/ItemBuilder.java
@@ -0,0 +1,183 @@
+package nerdex.io.misc;/*
+ * SpigotUtils
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.Color;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ItemBuilder {
+
+    private ItemStack is;
+
+    public ItemBuilder(Material m){
+        this(m, 1);
+    }
+
+    public ItemBuilder(ItemStack is){
+        this.is=is;
+    }
+
+    public ItemBuilder(Material m, int amount){
+        is= new ItemStack(m, amount);
+    }
+
+    public ItemBuilder(Material m, int amount, byte durability){
+        is = new ItemStack(m, amount, durability);
+    }
+
+    public ItemBuilder clone(){
+        return new ItemBuilder(is);
+    }
+
+    public ItemBuilder setDurability(short dur){
+        is.setDurability(dur);
+        return this;
+    }
+
+    public ItemBuilder setName(String name){
+        ItemMeta im = is.getItemMeta();
+        im.setDisplayName(name);
+        is.setItemMeta(im);
+        return this;
+    }
+
+    public ItemBuilder addUnsafeEnchantment(Enchantment ench, int level){
+        is.addUnsafeEnchantment(ench, level);
+        return this;
+    }
+
+    public ItemBuilder removeEnchantment(Enchantment ench){
+        is.removeEnchantment(ench);
+        return this;
+    }
+
+    public ItemBuilder setSkullOwner(String owner){
+        try{
+            SkullMeta im = (SkullMeta)is.getItemMeta();
+            im.setOwner(owner);
+            is.setItemMeta(im);
+        }catch(ClassCastException expected){}
+        return this;
+    }
+
+    public ItemBuilder addEnchant(Enchantment ench, int level){
+        ItemMeta im = is.getItemMeta();
+        im.addEnchant(ench, level, true);
+        is.setItemMeta(im);
+        return this;
+    }
+
+    public ItemBuilder addEnchantments(Map<Enchantment, Integer> enchantments){
+        is.addEnchantments(enchantments);
+        return this;
+    }
+
+    public ItemBuilder setInfinityDurability(){
+        is.setDurability(Short.MAX_VALUE);
+        return this;
+    }
+
+    public ItemBuilder setLore(String... lore){
+        ItemMeta im = is.getItemMeta();
+        im.setLore(Arrays.asList(lore));
+        is.setItemMeta(im);
+        return this;
+    }
+
+    public ItemBuilder setLore(List<String> lore) {
+        ItemMeta im = is.getItemMeta();
+        im.setLore(lore);
+        is.setItemMeta(im);
+        return this;
+    }
+    public ItemBuilder removeLoreLine(String line){
+        ItemMeta im = is.getItemMeta();
+        List<String> lore = new ArrayList<>(im.getLore());
+        if(!lore.contains(line))return this;
+        lore.remove(line);
+        im.setLore(lore);
+        is.setItemMeta(im);
+        return this;
+    }
+
+    public ItemBuilder removeLoreLine(int index){
+        ItemMeta im = is.getItemMeta();
+        List<String> lore = new ArrayList<>(im.getLore());
+        if(index<0||index>lore.size())return this;
+        lore.remove(index);
+        im.setLore(lore);
+        is.setItemMeta(im);
+        return this;
+    }
+
+    public ItemBuilder addLoreLine(String line){
+        ItemMeta im = is.getItemMeta();
+        List<String> lore = new ArrayList<>();
+        if(im.hasLore())lore = new ArrayList<>(im.getLore());
+        lore.add(line);
+        im.setLore(lore);
+        is.setItemMeta(im);
+        return this;
+    }
+
+    public ItemBuilder addLoreLine(String line, int pos){
+        ItemMeta im = is.getItemMeta();
+        List<String> lore = new ArrayList<>(im.getLore());
+        lore.set(pos, line);
+        im.setLore(lore);
+        is.setItemMeta(im);
+        return this;
+    }
+
+    @SuppressWarnings("deprecation")
+    public ItemBuilder setDyeColor(DyeColor color){
+        this.is.setDurability(color.getWoolData());
+        return this;
+    }
+
+    @Deprecated
+    public ItemBuilder setWoolColor(DyeColor color){
+        if(!is.getType().equals(Material.WOOL))return this;
+        this.is.setDurability(color.getWoolData());
+        return this;
+    }
+
+    public ItemBuilder setLeatherArmorColor(Color color){
+        try{
+            LeatherArmorMeta im = (LeatherArmorMeta)is.getItemMeta();
+            im.setColor(color);
+            is.setItemMeta(im);
+        }catch(ClassCastException expected){}
+        return this;
+    }
+
+    public ItemStack toItemStack(){
+        return is;
+    }
+}
+
