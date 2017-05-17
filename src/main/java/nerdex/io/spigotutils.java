f813b342a2b1fbbd49373c9f870d174a46ba9af2
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-17 19:10:21 +0100

    initial commit

---
+++ src/main/java/nerdex/io/spigotutils.java
@@ -0,0 +1,15 @@
+package nerdex.io;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class spigotutils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
