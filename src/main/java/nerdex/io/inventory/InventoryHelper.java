ce8a43bb5fa3f0ac0312f3ed15b389973a2b87ab
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-17 19:19:25 +0100

    working on inventory helper

---
+++ src/main/java/nerdex/io/inventory/InventoryHelper.java
@@ -0,0 +1,41 @@
+package nerdex.io.inventory;/*
+ * SpigotUtils
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+public class InventoryHelper {
+
+    public static void removeItemFromInventory(PlayerInventory inventory, Material type, int amount){
+        for(ItemStack is : inventory.getContents()){
+            if(is != null && is.getType() == type){
+                int newAmount = is.getAmount() - amount;
+                if(newAmount > 0){
+                    is.setAmount(newAmount);
+                    break;
+                } else {
+                    inventory.remove(is);
+                    amount = -newAmount;
+                    if (amount == 0) break;
+                }
+            }
+        }
+    }
+
+}
