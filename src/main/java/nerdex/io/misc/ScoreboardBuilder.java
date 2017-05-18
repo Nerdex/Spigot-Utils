5b2f13450b6d69509b28b0ee2401ed0dc19346e0
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-18 18:33:46 +0100

    working on scoreboard builder

---
+++ src/main/java/nerdex/io/misc/ScoreboardBuilder.java
@@ -0,0 +1,46 @@
+package nerdex.io.misc;/*
+ * SpigotUtils
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+import java.util.List;
+import java.util.Map;
+
+public class ScoreboardBuilder {
+
+    private static Scoreboard scoreboard;
+
+    private static String title;
+    private static Map<String, Integer> scores;
+    private List<Team> teams;
+
+    public ScoreboardBuilder(String title){
+        this.scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        this.title = title;
+        this.scores = Maps.newLinkedHashMap();
+        this.teams = Lists.newArrayList();
+    }
+
+    public static void blankeLine(){
+        
+    }
+}
