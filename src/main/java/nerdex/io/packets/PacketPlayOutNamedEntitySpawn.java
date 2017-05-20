36ffc995653cf5ebf1145e9f11acf3c29ca2af55
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-20 01:36:33 +0100

    fuck reflection

---
+++ src/main/java/nerdex/io/packets/PacketPlayOutNamedEntitySpawn.java
@@ -0,0 +1,36 @@
+package nerdex.io.packets;/*
+ * SpigotUtils
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.v1_11_R1.DataWatcher;
+
+import java.util.List;
+
+public class PacketPlayOutNamedEntitySpawn {
+
+    private int a;
+    private GameProfile profile;
+    private int c;
+    private int d;
+    private int e;
+    private byte f;
+    private byte g;
+    private int h;
+    private DataWatcher i;
+    private List j;
+}
